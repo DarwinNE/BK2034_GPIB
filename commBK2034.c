2628ea00dfb10643e1d6029273b27abf0e139351
DarwinNE <davbucci@gmail.com>
2018-01-23 23:56:44 +0100

    Added interactive version in HP-IB for an HP9816 computer.

--- commBK2034.c
+++ commBK2034.c
@@ -1,8 +1,4 @@
-#ifdef __MACH__
-#include <NI488/ni488.h>
-#else
-#include "ni488.h"
-#endif
+
 #include <stdio.h>
 #include <strings.h>
 #include <stdlib.h>
@@ -10,44 +6,21 @@
 #include <time.h>
 #include <math.h>
 
-
+#include "GPIB.h"
 #include "commBK2034.h"
 
-
-int Device = 0;                   /* Device unit descriptor                  */
-int BoardIndex = 0;
-
-void setBoardGPIB(int board)
-{
-    BoardIndex = board;
-}
-
-void writeGPIB(char *command)
-{
-     ibwrt(Device, command, strlen(command));
-}
-
-void writenGPIB(char *command, size_t numbytes)
-{
-     ibwrt(Device, command, numbytes);
-}
-
-char *readGPIB(char *buffer, size_t maxlen)
-{
-    ibrd(Device, buffer, maxlen);
-    return buffer;
-}
+#define BUFSIZE 1001
 
 /** Configure the acquisition of the BK 2034 to acquire the
     wanted spectrum and number of averages.
 */
 void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
 {
-    char command[1001];
+    char command[BUFSIZE];
 
     /* Show upper graph and measurement settings */
     sprintf(command, "DISPLAY_FORMAT UM\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     /* System settings for MEASUREMENT */
     /* Configure the acquisition channel, at first. */
@@ -64,20 +37,20 @@ void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
             sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION MC 2\n");
             break;
     }
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION MM 0\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION ZB 1\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION AT 1\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION AN %d\n",navg);
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION AA 0\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION AW 1\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     /* System settings for DISPLAY */
 
@@ -97,16 +70,16 @@ void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
             break;
     }
 
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     if(logY) {
         sprintf(command, "EDIT_DISPLAY_SPECIFICATION YU 1\n");
-        ibwrt(Device, command, strlen(command));
+        writeGPIB(command);
         //writeGPIB("EDIT_DISPLAY_SPECIFICATION YL 1\n");
     } else {
         sprintf(command, "EDIT_DISPLAY_SPECIFICATION YU 0\n");
         /* Linear axes */
-        //ibwrt(Device, command, strlen(command));
+        //writeGPIB(command);
         //writeGPIB("EDIT_DISPLAY_SPECIFICATION YL 0\n");
     }
 
@@ -114,11 +87,11 @@ void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
         default:
         case CH_A:
             sprintf(command, "EDIT_DISPLAY_SPECIFICATION SU 2\n");
-            ibwrt(Device, command, strlen(command));
+            writeGPIB(command);
             break;
         case CH_B:
             sprintf(command, "EDIT_DISPLAY_SPECIFICATION SU 2\n");
-            ibwrt(Device, command, strlen(command));
+            writeGPIB(command);
             break;
         case H1:
             break;
@@ -126,12 +99,10 @@ void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
             break;
     }
 
-
-
     sprintf(command, "EDIT_DISPLAY_SPECIFICATION ID 2\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "EDIT_DISPLAY_SPECIFICATION DS 0\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
 }
 
@@ -140,22 +111,22 @@ void configureAcquisitionAndGraph2034(int navg, t_style s, bool logY)
 */
 void configureMaxFreq2034(char *maxfreq)
 {
-    char command[1001];
+    char command[BUFSIZE];
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION FS %s\n", maxfreq);
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 }
 
 /** Start a measurement campaign on the 2034.
 */
 void startMeasurement2034(void)
 {
-    char command[1001];
+    char command[BUFSIZE];
     sprintf(command, "KEY_PUSH G 0\n"); /* RECORD Cont */
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "KEY_PUSH C 0\n");   /* AVERAGING Stop */
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     sprintf(command, "KEY_PUSH A 0\n");   /* AVERAGING Start */
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     printf("Please start measurements for averaging.\n");
 }
 
@@ -163,19 +134,14 @@ void startMeasurement2034(void)
 */
 float readMaxFrequency2034(void)
 {
-    char  Buffer[1001];
-    char command[1001];
+    char  buffer[BUFSIZE];
+    char command[BUFSIZE];
     sprintf(command, "EDIT_MEASUREMENT_SPECIFICATION FREQUENCY_SPAN\n");
-    ibwrt(Device, command, strlen(command));
-    ibrd(Device, Buffer, sizeof(Buffer));
-
-    if (ibsta & ERR) {
-        GpibError("ibrd Error");
-    }
-    Buffer[ibcntl] = '\0';         /* Null terminate the ASCII string  */
+    writeGPIB(command);
+    readGPIB(buffer, BUFSIZE);
 
     float maxfreq;
-    sscanf(Buffer, "%f", &maxfreq);
+    sscanf(buffer, "%f", &maxfreq);
     printf("Frequency span: %5.1f Hz\n", maxfreq);
     return maxfreq;
 }
@@ -185,28 +151,23 @@ float readMaxFrequency2034(void)
 */
 void waitUntilFinished2034(int wnavg)
 {
-    char  Buffer[1001];
-    char command[1001];
+    char  buffer[BUFSIZE];
+    char command[BUFSIZE];
     float averagingnum;
     float wstar=50.0f;
     float complete=0.0f;
     int i;
 
     sprintf(command, "PROMPT 'PLEASE START MEASUREMENTS NOW'\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
     printf("[                                                  ]\r");
     do {
         waitFor(1);
         sprintf(command, "CURRENT_STATUS A_N_R\n");
-        ibwrt(Device, command, strlen(command));
-        ibrd(Device, Buffer, sizeof(Buffer));
+        writeGPIB(command);
+        readGPIB(buffer, BUFSIZE);
 
-        if (ibsta & ERR) {
-            GpibError("ibrd Error");
-        }
-        Buffer[ibcntl] = '\0';         /* Null terminate the ASCII string  */
-
-        sscanf(Buffer, "%f", &averagingnum);
+        sscanf(buffer, "%f", &averagingnum);
 
         complete=averagingnum/wnavg;
         printf("\r[");
@@ -214,7 +175,7 @@ void waitUntilFinished2034(int wnavg)
         /* Erase the prompt line on the 2034 */
         if(averagingnum>0) {
             sprintf(command, "PROMPT\n");
-            ibwrt(Device, command, strlen(command));
+            writeGPIB(command);
         }
         for(i=0; i<(int)(complete*wstar);++i) {
             printf("*");
@@ -238,14 +199,14 @@ void drawBandsOn2034(int octn, /* Number of bands */
     float *calcvalues,           /* Power in dB/U integrated in bands */
     float vrange)                /* Vertical range in dB */
 {
-    char command[1001];
+    char command[BUFSIZE];
     int i;
 
     sprintf(command, "WRITE_TEXT CLEAR_HOME\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     sprintf(command, "WRITE_TEXT 1,5,\"OCTAVE ANALYSIS, DAVIDE BUCCI 2015\"\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
 
 
@@ -269,21 +230,21 @@ void drawBandsOn2034(int octn, /* Number of bands */
     sprintf(command, "WRITE_TEXT 2,5,\"V. range: %4.2f dB, TOP: %4.2f dB\"\n",
         vrange, max);
 
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     // 401 pts/line, bipolar display, show scales
     sprintf(command, "CONTROL_PROCESS DISPLAY_MODE 1,0,1\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     sprintf(command, "PLOT_FOREGROUND 0,0\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     for(i =0; i<octn; ++i) {
         val=(int)((calcvalues[i]-max)*mult)+baseline;
 
         sprintf(command, "PLOT_CONTINUE_FOREGROUND %d,%d,%d,%d\n",
             (int)(octwidth*i),val,(int)(octwidth*(i+1)-1),val);
-        ibwrt(Device, command, strlen(command));
+        writeGPIB(command);
 
         octaveinflimit=limits[i];
         octavesuplimit=limits[i+1];
@@ -303,7 +264,7 @@ void drawBandsOn2034(int octn, /* Number of bands */
                 (int)(octwidth*(i+0.1f)-corr),20,
                 (int)round(centraloct));
         }
-        ibwrt(Device, command, strlen(command));
+        writeGPIB(command);
     }
 }
 
@@ -322,8 +283,8 @@ int getBandsFrom2034(
     char *filename)                     /* Name of the file where to store raw
                                            data (NULL: do not store anything)*/
 {
-    char command[1001];
-    char  Buffer[1001];
+    char command[BUFSIZE];
+    char  buffer[BUFSIZE];
 
     float centraloct=16.0f;
     int ptinoctave=0;
@@ -340,7 +301,7 @@ int getBandsFrom2034(
     float deltaf=maxfreq/((float)npoints-1);
     /* This increases the execution speed of the transfer via GPIB */
     sprintf(command, "CONTROL_PROCESS MAXIMUM_INTERFACE_ACTIVITY\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     if(filename!=NULL)
         fout=fopen(filename, "w");
@@ -349,13 +310,9 @@ int getBandsFrom2034(
     for (i=0; i<npoints; ++i) {
         freq=deltaf*(float)(i);
         sprintf(command, "AF IR,%d\n", i);
-        ibwrt(Device, command, strlen(command));
-        ibrd(Device, Buffer, sizeof(Buffer));
-        if (ibsta & ERR) {
-            GpibError("ibrd Error");
-        }
-        Buffer[ibcntl] = '\0';
-        sscanf(Buffer+1,"%f", &value);
+        writeGPIB(command);
+        readGPIB(buffer, BUFSIZE);
+        sscanf(buffer+1,"%f", &value);
 
         if(fout!=NULL) {
             fprintf(fout,"%f %f\n",freq, value);
@@ -407,7 +364,7 @@ int getBandsFrom2034(
 
     printf("Total power: %f dB/YREF*Hz\n",10*log10(total));
     sprintf(command, "CONTROL_PROCESS NORMAL_INTERFACE_ACTIVITY\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     return octn;
 }
@@ -424,8 +381,8 @@ int getDataPoints2034(
     float *storage,     /* Array where read data will be stored */
     int npoints)        /* Number of points to be read */
 {
-    char command[1001];
-    char  Buffer[1001];
+    char command[BUFSIZE];
+    char buffer[BUFSIZE];
 
     float value;
 
@@ -436,19 +393,14 @@ int getDataPoints2034(
 
     /* This increases the execution speed of the transfer via GPIB */
     sprintf(command, "CONTROL_PROCESS MAXIMUM_INTERFACE_ACTIVITY\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     for (i=0; i<npoints; ++i) {
         freqs[i]=deltaf*(float)(i);
         sprintf(command, "AF IR,%d\n", i);
-        ibwrt(Device, command, strlen(command));
-        ibrd(Device, Buffer, sizeof(Buffer));
-        if (ibsta & ERR) {
-            GpibError("ibrd Error");
-            return i;
-        }
-        Buffer[ibcntl] = '\0';
-        sscanf(Buffer+1,"%f", &value);
+        writeGPIB(command);
+        readGPIB(buffer, BUFSIZE);
+        sscanf(buffer+1,"%f", &value);
         storage[i]=value;
         if(i%15==0) {
             printf(".");
@@ -457,7 +409,7 @@ int getDataPoints2034(
     }
     printf("\n");
     sprintf(command, "CONTROL_PROCESS NORMAL_INTERFACE_ACTIVITY\n");
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
     return npoints;
 }
@@ -466,27 +418,14 @@ int getDataPoints2034(
 */
 void init2034(int boardIndex, int primaryAddress, int secondaryAddress)
 {
-    char  Buffer[1001];
-    char command[1001];
+    char  buffer[BUFSIZE];
+    char command[BUFSIZE];
 
-    Device = ibdev(               /* Create a unit descriptor handle         */
-         boardIndex,              /* Board Index (GPIB0 = 0, GPIB1 = 1, ...) */
-         primaryAddress,          /* Device primary address                  */
-         secondaryAddress,        /* Device secondary address                */
-         T3s,                     /* Timeout setting (T3s = 3 seconds)     */
-         1,                       /* Assert EOI line at end of write         */
-         0);                      /* EOS termination mode                    */
-    if (ibsta & ERR) {            /* Check for GPIB Error                    */
-        GpibError("ibdev Error");
-    }
+    initGPIB(boardIndex, primaryAddress, secondaryAddress);
 
-    ibclr(Device);                 /* Clear the device                       */
-    if (ibsta & ERR) {
-        GpibError("ibclr Error");
-    }
     /* Set the line terminator for the device */
     strncpy(command, "DEFINE_TERMINATOR \n", sizeof(command));
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 }
 
 /** Read an identification string from the 2034.
@@ -496,106 +435,26 @@ void init2034(int boardIndex, int primaryAddress, int secondaryAddress)
 */
 void identify2034(void)
 {
-    char command[1001];
-    char  Buffer[1001];
+    char command[BUFSIZE];
+    char  buffer[BUFSIZE];
     // Get the device identification.
     strncpy(command, "IDENTIFY\n", sizeof(command));
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 
-    ibrd(Device, Buffer, sizeof(Buffer));
-    if (ibsta & ERR) {
-        GpibError("ibrd Error");
+    if(readGPIB(buffer, BUFSIZE)==NULL) {
         exit(1);    /* A little brutal but effective */
     }
-    Buffer[ibcntl] = '\0';         /* Null terminate the ASCII string         */
 
-    printf("Identification received: %s", Buffer);
+    printf("Identification received: %s", buffer);
 }
 
 /** Level 2 reset of the 2034
 */
 void reset2lev2034(void)
 {
-    char command[1001];
+    char command[BUFSIZE];
     /* Reset the device */
     strncpy(command, "SYSTEM_RESET 2\n", sizeof(command));
-    ibwrt(Device, command, strlen(command));
+    writeGPIB(command);
 }
 
-
-/**  Un-initialization - Done only once at the end of your application.
-*/
-void closeCommIEEE(void)
-{
-
-   ibonl(Device, 0);              /* Take the device offline                 */
-   if (ibsta & ERR) {
-      GpibError("ibonl Error");
-   }
-
-   ibonl(BoardIndex, 0);          /* Take the interface offline              */
-   if (ibsta & ERR) {
-      GpibError("ibonl Error");
-   }
-}
-
-/*****************************************************************************
- *                      Function GPIBERROR
- * This function will notify you that a NI-488 function failed by
- * printing an error message.  The status variable IBSTA will also be
- * printed in hexadecimal along with the mnemonic meaning of the bit
- * position. The status variable IBERR will be printed in decimal
- * along with the mnemonic meaning of the decimal value.  The status
- * variable IBCNTL will be printed in decimal.
- *
- * The NI-488 function IBONL is called to disable the hardware and
- * software.
- *
- * The EXIT function will terminate this program.
- *****************************************************************************/
-void GpibError(char *msg) {
-
-    printf ("%s\n", msg);
-
-    printf ("ibsta = 0x%x  <", ibsta);
-    if (ibsta & ERR )  printf (" ERR");
-    if (ibsta & TIMO)  printf (" TIMO");
-    if (ibsta & END )  printf (" END");
-    if (ibsta & SRQI)  printf (" SRQI");
-    if (ibsta & RQS )  printf (" RQS");
-    if (ibsta & CMPL)  printf (" CMPL");
-    if (ibsta & LOK )  printf (" LOK");
-    if (ibsta & REM )  printf (" REM");
-    if (ibsta & CIC )  printf (" CIC");
-    if (ibsta & ATN )  printf (" ATN");
-    if (ibsta & TACS)  printf (" TACS");
-    if (ibsta & LACS)  printf (" LACS");
-    if (ibsta & DTAS)  printf (" DTAS");
-    if (ibsta & DCAS)  printf (" DCAS");
-    printf (" >\n");
-
-    printf ("iberr = %d", iberr);
-    if (iberr == EDVR) printf (" EDVR <System Error>\n");
-    if (iberr == ECIC) printf (" ECIC <Not Controller-In-Charge>\n");
-    if (iberr == ENOL) printf (" ENOL <No Listener>\n");
-    if (iberr == EADR) printf (" EADR <Address error>\n");
-    if (iberr == EARG) printf (" EARG <Invalid argument>\n");
-    if (iberr == ESAC) printf (" ESAC <Not System Controller>\n");
-    if (iberr == EABO) printf (" EABO <Operation aborted>\n");
-    if (iberr == ENEB) printf (" ENEB <No GPIB board>\n");
-    if (iberr == EOIP) printf (" EOIP <Async I/O in progress>\n");
-    if (iberr == ECAP) printf (" ECAP <No capability>\n");
-    if (iberr == EFSO) printf (" EFSO <File system error>\n");
-    if (iberr == EBUS) printf (" EBUS <GPIB bus error>\n");
-    if (iberr == ESTB) printf (" ESTB <Status byte lost>\n");
-    if (iberr == ESRQ) printf (" ESRQ <SRQ stuck on>\n");
-    if (iberr == ETAB) printf (" ETAB <Table Overflow>\n");
-
-    printf ("\n");
-    printf ("ibcntl = %ld\n", ibcntl);
-    printf ("\n");
-
-    /* Call ibonl to take the device and interface offline */
-    //ibonl (Device,0);
-    //ibonl (BoardIndex,0);
-}
--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 
 all : octave totaldoc impedance record
 
-octave : octave.c commBK2034.c commBK2034.h
-	gcc octave.c commBK2034.c -m32 -framework NI488 -o octave
+octave : octave.c commBK2034.c commBK2034.h GPIB.c GPIB.h
+	gcc octave.c commBK2034.c GPIB.c -m32 -framework NI488 -o octave
 	
-totaldoc : totaldoc.c commBK2034.c commBK2034.h
-	gcc totaldoc.c commBK2034.c -m32 -framework NI488 -o totaldoc
+totaldoc : totaldoc.c commBK2034.c commBK2034.h GPIB.c GPIB.h
+	gcc totaldoc.c commBK2034.c GPIB.c -m32 -framework NI488 -o totaldoc
 	
-impedance : impedance.c commBK2034.c commBK2034.h
-	gcc impedance.c commBK2034.c -m32 -framework NI488 -o impedance
+impedance : impedance.c commBK2034.c commBK2034.h GPIB.c GPIB.h
+	gcc impedance.c commBK2034.c GPIB.c -m32 -framework NI488 -o impedance
 
-record : record.c commBK2034.c commBK2034.h
-	gcc record.c commBK2034.c -m32 -framework NI488 -o record
+record : record.c commBK2034.c commBK2034.h GPIB.c GPIB.h
+	gcc record.c commBK2034.c GPIB.c -m32 -framework NI488 -o record
 
 	
 clean :
-	rm octave 
+	rm octave totaldoc impedance record
